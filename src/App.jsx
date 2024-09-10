f083bde635915724c7c16a5ebe7f61d3ed1e3585
Gonzalo <gonzaloduque4@gmail.com>
2024-09-10 00:32:05 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
-        <Router basename="/React-ecommerce">  {/*configurar basement aca*/}
+        <Router basename="/React-ecommerce">  {}
             <NavBar />
             <Routes>
                 <Route path="/" element={<ItemListContainer />} />
